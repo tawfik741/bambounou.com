5738fbef990ba27904a2bf6781d9554e2a206509
abenheni <a.benheni@gmail.com>
2021-03-04 19:29:26 +0100

    feat: adding styles to center components responsively

--- src/components/footer/content.jsx
+++ src/components/footer/content.jsx
@@ -1,53 +1,95 @@
-import React, { useState } from "react"
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
-import { faSoundcloud, faBandcamp, faSpotify, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { ButtonGroup, IconButton, Typography, Modal, useMediaQuery } from "@material-ui/core";
+import {
+  faSoundcloud,
+  faBandcamp,
+  faSpotify,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  ButtonGroup,
+  IconButton,
+  Typography,
+  Modal,
+  useMediaQuery,
+} from "@material-ui/core";
 
 export const Footer = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-   // const isLarge = useMediaQuery('(min-width:600px)');
-    
-    //<IconButton onClick={() => setOpen(true)}><StyledIcon icon={faQuestionCircle} size='lg' />{/* {isLarge ? null : 'sometext'} */}</IconButton>
-        
-    const renderButtonsList = () => {
-        return (<>
-            <IconButton href={`https://www.instagram.com/bambounouu/?hl=fr`} target='_blank'><StyledIcon size='lg' icon={faInstagram} />{/* {isLarge ? null : 'sometext'} */}</IconButton>
-            <IconButton href={`https://bambounouofficial.bandcamp.com/`} ><StyledIcon size='lg' icon={faBandcamp} />{/* {isLarge ? null : 'sometext'} */}</IconButton>
-            <IconButton href={`https://soundcloud.com/BAMBOUNOU`}><StyledIcon size='lg' icon={faSoundcloud} />{/* {isLarge ? null : 'sometext'} */}</IconButton>
-            <IconButton href={`https://open.spotify.com/artist/48P6su5lHrVCZtJVXMjJnX`}><StyledIcon size='lg' icon={faSpotify} />{/* {isLarge ? null : 'sometext'} */}</IconButton>
-        </>)
-    }
-    /* if (isLarge) */ return (
-        <>
-            <ButtonGroup style={{ position: 'fixed', bottom: 20, right: 20 }} size='large'>
-                {renderButtonsList()}
-            </ButtonGroup>
-            <Modal open={open} onClose={() => setOpen(false)}>
-                <div style={{ backgroundColor: 'purple', width: 530, height: 163 }}>
-                    <Typography>About us</Typography>
-                </div>
-            </Modal>
-        </>
-    )
-/*     return (
+  // const isLarge = useMediaQuery('(min-width:600px)');
+
+  //<IconButton onClick={() => setOpen(true)}><StyledIcon icon={faQuestionCircle} size='lg' />{/* {isLarge ? null : 'sometext'} */}</IconButton>
+
+  const renderButtonsList = () => {
+    return (
+      <>
+        <IconButton
+          href={`https://www.instagram.com/bambounouu/?hl=fr`}
+          target="_blank"
+        >
+          <StyledIcon size="lg" icon={faInstagram} />
+          {/* {isLarge ? null : 'sometext'} */}
+        </IconButton>
+        <IconButton href={`https://bambounouofficial.bandcamp.com/`}>
+          <StyledIcon size="lg" icon={faBandcamp} />
+          {/* {isLarge ? null : 'sometext'} */}
+        </IconButton>
+        <IconButton href={`https://soundcloud.com/BAMBOUNOU`}>
+          <StyledIcon size="lg" icon={faSoundcloud} />
+          {/* {isLarge ? null : 'sometext'} */}
+        </IconButton>
+        <IconButton
+          href={`https://open.spotify.com/artist/48P6su5lHrVCZtJVXMjJnX`}
+        >
+          <StyledIcon size="lg" icon={faSpotify} />
+          {/* {isLarge ? null : 'sometext'} */}
+        </IconButton>
+      </>
+    );
+  };
+  /* if (isLarge) */ return (
+    <>
+      <StyledGroup style={{}} size="large">
+        {renderButtonsList()}
+      </StyledGroup>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={{ backgroundColor: "purple", width: 530, height: 163 }}>
+          <Typography>About us</Typography>
+        </div>
+      </Modal>
+    </>
+  );
+  /*     return (
         <ButtonGroup style={{ position: 'fixed', top: '40%', left: '40%' }} orientation='vertical'>
             {renderButtonsList()}
         </ButtonGroup>
     ) */
-}
+};
+
+const StyledGroup = styled(ButtonGroup)`
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    right: 0px;
+    justify-content: space-evenly;
+  }
+`;
 
 const StyledIcon = styled(FontAwesomeIcon)`
-    color: #707080;
-    margin: 13px 40px;
-    
-    @media (max-width: 800px) {
-        margin: 0 45px;
-    }
-    @media (max-width: 600px) {
-        margin: 0 8px;
-    }
- `
+  color: #707080;
+  margin: 13px 40px;
+
+  @media (max-width: 800px) {
+    margin: 0 45px;
+  }
+  @media (max-width: 600px) {
+    margin: 0 8px;
+  }
+`;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,59 +1,61 @@
 import React from "react";
 import P5 from "../components/background/p5";
-import {Footer} from "../components/footer/content"
-import styled from "styled-components"
-import bambe from "../images/bambe-purple.png"
+import { Footer } from "../components/footer/content";
+import styled from "styled-components";
+import bambe from "../images/bambe-purple.png";
 
-import "../styles/fonts.css"
+import "../styles/fonts.css";
 
 const IndexPage = () => {
-    return (<div>
-        <HeaderIcon >_Bambounou</HeaderIcon>
-        <BambeIcon><img src={bambe} alt=""/></BambeIcon>
-        
-        <P5/>
-        <Footer/>
+  return (
+    <div>
+    <Padding>
+      <Header>
+        <HeaderIcon>_Bambounou</HeaderIcon>
+        <BambeIcon>
+          <img src={bambe} alt="" />
+        </BambeIcon>
+      </Header>
+
+    </Padding>
+
+      <P5 />
+      <Footer />
     </div>
-    )
+  );
+};
 
-}
+const Padding = styled.div`
+    padding: 16px;
+`;
 
-const HeaderIcon = styled('div')`
-    color: #707080;
-    
-    font-size:60px;
-    margin: 13px 40px;
-    
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
 
-    @media (max-width: 800px) {
-        font-size:70px;
-        margin: 13px 45px;
-    }
-    @media (max-width: 600px) {
-        font-size:40px;
-        margin: 30px 8px;
-    }
- `
+  @media (max-width: 600px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
+const HeaderIcon = styled("div")`
+  color: #707080;
 
-const BambeIcon = styled('div')`
-color: #000;
+  font-size: 60px;
+  line-height: 0.8;
 
-font-size:60px;
-margin: 13px 40px;
+  @media (max-width: 600px) {
+    font-size: 40px;
+    padding: 40px 0px;
+  }
+`;
 
-position : fixed;
-left : 78vw;
-top : -5vh;
-
-
-@media (max-width: 800px) {
-    font-size:70px;
-    margin: 13px 45px;
-}
-@media (max-width: 600px) {
-    font-size:40px;
-    margin: 30px 8px;
-}
-`
-export default IndexPage
+const BambeIcon = styled("div")`
+  color: #000;
+`;
+export default IndexPage;
